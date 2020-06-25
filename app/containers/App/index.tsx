02bdae315154f6874723ae3b0a45e57994ea139d
djordje <djordje@snowball.digital>
2020-06-25 12:46:35 +0200

    here

--- app/containers/App/index.tsx
+++ app/containers/App/index.tsx
@@ -11,24 +11,36 @@ import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
 
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from "apollo-boost";
+
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import NavBar from 'components/nav-bar';
 import PageWrapper from 'components/page-wrapper';
 
 import GlobalStyle from '../../global-styles';
+import TestView from './../Test'
+
+const client = new ApolloClient({
+  uri: `https://myokaycash-backend.herokuapp.com/graphql`
+});
+
 function App() {
   return (
-    <div>
-      <NavBar/>
-      <PageWrapper >
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </PageWrapper>
-      <GlobalStyle />
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        {/* <NavBar/>
+        <PageWrapper >
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </PageWrapper>
+        <GlobalStyle /> */}
+        <TestView/>
+      </div>
+    </ApolloProvider>
   );
 }
 export default hot(App);
--- app/containers/HomePage/view.tsx
+++ app/containers/HomePage/view.tsx
@@ -1,38 +1,83 @@
+
 import { FormStateHandler } from './types';
-import React from 'react';
+import React, { useLayoutEffect, useState, useEffect }from 'react';
 import { TextContainer, PageContainer, InputContainer, ButtonContainer, Section } from './styles';
 
+import { ApolloProvider, useMutation } from '@apollo/react-hooks';
+import gql from "graphql-tag";
+import { useQuery } from '@apollo/react-hooks';
+
+const userData = gql`
+    mutation signUp ($input: UserLogin! ){
+      signUp(input: $input){
+        code
+        qrCode
+      }
+    }
+  `;
+
 function HomePageView({ form }: { form: FormStateHandler }) {
+  // const [data2, setData2] = useState();
+  const [createuser, { data, loading, error }] = useMutation(userData);
+
+  // const [createuser, { data, loading, error }] = useMutation(
+  //   userData,
+  //   {
+  //     update(cache, { data: { addTodo } }) {
+  //       // const { todos } = cache.readQuery({ query: GET_TODOS });
+  //       // cache.writeQuery({
+  //       //   query: GET_TODOS,
+  //       //   data: { todos: todos.concat([addTodo]) },
+  //       // });
+
+  //       // setData2(data);
+  //     // console.log('********data2',data)
+  //     }
+  //   }
+  // );
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  
+
+  // useEffect(( ) => {
+  //       // const res = createuser({variables:{input: { username: "test667s56sss343s7889", password: "342asdasd424" }}});
+
+  // },[data]);
+
+  console.log('********data2',data)
+
   return (
-    <form onSubmit={form.handleSubmit}>
-        <PageContainer>
-            <Section>
-                <TextContainer>
-                <h1>Create your account on My<span>OKAY</span>Cash</h1>
-                <h5>Enter you credentials to create your account</h5>
-                </TextContainer>
-                <InputContainer>
-                    <div>
-                        <h3>Name</h3>
-                        <input
-                            value={form.username}
-                            onChange={form.onUsernameChange}
-                        />
-                    </div>
-                    <div>
-                        <h3>Password</h3>
-                        <input
-                            value={form.password}
-                            onChange={form.onPasswordChange}
-                        />
-                    </div>
-                </InputContainer>
-            </Section>
-            <ButtonContainer>
-                <button type={'submit'}>ACTIVATE</button>
-            </ButtonContainer>
-        </PageContainer>
-    </form>
+      <h1>Ok</h1>
+    // <form onSubmit={form.handleSubmit}>
+    //     <PageContainer>
+    //         <Section>
+    //             <TextContainer>
+    //             <h1>Create your account on My<span>OKAY</span>Cash</h1>
+    //             <h5>Enter you credentials to create your account</h5>
+    //             </TextContainer>
+    //             <InputContainer>
+    //                 <div>
+    //                     <h3>Name</h3>
+    //                     <input
+    //                         value={form.username}
+    //                         onChange={form.onUsernameChange}
+    //                     />
+    //                 </div>
+    //                 <div>
+    //                     <h3>Password</h3>
+    //                     <input
+    //                         value={form.password}
+    //                         onChange={form.onPasswordChange}
+    //                     />
+    //                 </div>
+    //             </InputContainer>
+    //         </Section>
+    //         <ButtonContainer>
+    //             <button type={'submit'}>ACTIVATE</button>
+    //         </ButtonContainer>
+    //     </PageContainer>
+    // </form>
   );
 }
 
